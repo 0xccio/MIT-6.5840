fba0e56c072eaf149ad09cf597f21d4647520d0a
cc <c.c@u.nus.edu>
2025-05-25 15:20:37 +0800

    Initial commit: import 6.5840 labs from MIT

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -23,7 +23,13 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
+type Request struct {
 
+}
+
+type Response struct {
+	
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
