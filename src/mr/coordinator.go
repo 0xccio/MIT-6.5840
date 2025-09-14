d8fba5615a105c00a984283f84022e8b8a175fc8
cc <c.c@u.nus.edu>
2025-09-14 18:51:37 +0800

    feat: test rebase from dd

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -58,6 +58,7 @@ func (tc *TaskCollection) AddTask(taskMeta *TaskMetaInfo) {
 }
 
 func (tc *TaskCollection) GetTaskMetaInfo(taskId int) (*TaskMetaInfo, bool) {
+	fmt.Println("test rebase: 1 -> 3 from dd")
 	fmt.Println("test rebase: 1 -> 2 from cc")
 	res, err := tc.MetaMap[taskId]
 	return res, err
