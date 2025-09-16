bb87e1dcc4a25bccc07eef0859d62958974cd32d
cc <c.c@u.nus.edu>
2025-09-16 22:38:24 +0800

    Revert "feat: add from ee (#6)"
    This reverts commit ee86a1bb836e3fbafdcc566b35a4040db4ae5d13.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -58,7 +58,6 @@ func (tc *TaskCollection) AddTask(taskMeta *TaskMetaInfo) {
 
 func (tc *TaskCollection) GetTaskMetaInfo(taskId int) (*TaskMetaInfo, bool) {
 	fmt.Println("test rebase: 1 -> 2 from cc")
-	fmt.Println("test rebase: 1 -> 2 from ee")
 	res, err := tc.MetaMap[taskId]
 	return res, err
 }
