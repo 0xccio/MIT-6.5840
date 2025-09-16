8d59937c6f0b78c658b7e1664c4edb420b9d583b
cc <c.c@u.nus.edu>
2025-09-16 22:28:50 +0800

    Revert "feat: test rebase from dd"
    This reverts commit d8fba5615a105c00a984283f84022e8b8a175fc8.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -58,7 +58,6 @@ func (tc *TaskCollection) AddTask(taskMeta *TaskMetaInfo) {
 }
 
 func (tc *TaskCollection) GetTaskMetaInfo(taskId int) (*TaskMetaInfo, bool) {
-	fmt.Println("test rebase: 1 -> 3 from dd")
 	fmt.Println("test rebase: 1 -> 2 from cc")
 	fmt.Println("cc change this line")
 	res, err := tc.MetaMap[taskId]
